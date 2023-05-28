0b50e1029768cd8ef18581e826a526e28aaf984d
Darren LaCour <lacourd@icloud.com>
2023-05-28 00:24:53 -0500

    Chapter 18 Reading and exercises

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -14,6 +16,7 @@ public class Event extends AbstractEntity{
     @NotBlank(message = "Event Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
+
     @OneToOne(cascade = CascadeType.ALL)
     @Valid
     @NotNull
@@ -22,6 +25,10 @@ public class Event extends AbstractEntity{
     @ManyToOne
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
+
+    @ManyToMany
+    private final List<Tag> tags = new ArrayList<>();
+
     public Event(String name, EventCategory eventCategory) {
         this.name = name;
         this.eventCategory = eventCategory;
@@ -51,6 +58,14 @@ public class Event extends AbstractEntity{
 
     public void setEventDetails(EventDetails eventDetails) {
         this.eventDetails = eventDetails;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
     }
 
     @Override
